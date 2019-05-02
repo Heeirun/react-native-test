55292e6935d5f2aebaab6b3ff819f0049c46ade3
Heeirun Jayakumar <heeirunjayakumar@Heeiruns-MacBook-Pro-2.local>
2019-05-01 21:12:31 -0500

    Done The Basics until Style. Move on and focus on changing things around

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+export default class LotsOfStyles extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text>Hello World!</Text>
+      <View>
+        <Blink style={styles.red} text='just red'/>
+        <Blink style={styles.bigBlue}just text='bigBlue'/>
+        <Blink style={[styles.bigBlue, styles.red]} text='bigBlue, then red'/>
+        <Blink style={[styles.red, styles.bigBlue]} text='red, then bigBlue'/>
       </View>
     );
   }
 }
+
+// skip this line if using Create React Native App
